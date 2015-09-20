685aed1593ee0f15fb911d973773acf3862ba58e
Web Server <webserver@46.101.9.221>
2015-09-20 13:32:55 -0400

    Initial Commit

---
+++ html/includes/dbconnect.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<?php
+$server = "46.101.9.221"; // IP Address of the MySQL server
+$username = "phpaccess"; // Username to log into MySQL server
+$password = "xteUPG8wqQZ8yKzm"; // Password for phpaccess MySQL account (randomly generated
+$dbname = "project"; // Database to be used (currently the 'project' DB)
+
+//Initialise DB connection using above variables
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if DB connection was successful, if not display error
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+else {
+}
+?>
+
+</body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,49 @@
+<?php
+include 'includes/dbconnect.php'; // Adds the database connection script to this page
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SIGN ME IN</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+  <style>
+
+}
+    </style>
+ <body>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">SIGN ME IN</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+</html>
