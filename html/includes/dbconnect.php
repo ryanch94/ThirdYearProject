9ff6a08d5162cb473b9c168ff7d4f9b405aadd9c
Ryan Challis <s00143598@mail.itsligo.ie>
2015-09-23 10:52:23 +0100

    Push changes

--- html/includes/dbconnect.php
+++ html/includes/dbconnect.php
@@ -1,19 +1,18 @@
 <html>
 <body>
 <?php
-$servername = "localhost"; // IP Address of the MySQL server
-$username = "phpaccess"; // Username to log into MySQL server
-$password = "xteUPG8wqQZ8yKzm"; // Password for phpaccess MySQL account (randomly generated
-$dbname = "project"; // Database to be used (currently the 'project' DB)
+// Define constants
+define("MYSQL_ADDRESS", "localhost"); // IP Address of the MySQL server
+define("MYSQL_USERNAME", "phpaccess"); // Username to log into MySQL server
+define("MYSQL_PASS", "xteUPG8wqQZ8yKzm"); // Password for phpaccess MySQL account (randomly generated when user was created)
+define("MYSQL_DB", "project") // Database to be used (currently the 'project' DB)
 
 //Initialise DB connection using above variables
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect(MYSQL_ADDRESS, MYSQL_USERNAME, MYSQL_PASS, MYSQL_DB);
 
-// Check if DB connection was successful, if not display error
+// Check if DB connection was successful, if not kill the rest of the page load and display error
 if (!$conn) {
     die("MySQL Connection failed: " . mysqli_connect_error());
-}
-else {
 }
 ?>
 
--- html/index.php
+++ html/index.php
@@ -1,49 +1,112 @@
-<?php
-//include 'includes/dbconnect.php'; // Adds the database connection script to this page
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>SIGN ME IN</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-  <style>
+<html>
+  <head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
 
-}
-    </style>
- <body>
+    <title>Counter - Coming Soon Theme</title>
 
-    <nav class="navbar navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">SIGN ME IN</a>
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/soon.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+  </head>
+  <!-- START BODY -->
+  <body class="nomobile">
+
+    <!-- START HEADER -->
+    <section id="header">
+        <div class="container">
+            
+
+        <!--FORM FOR LOGIN -->
+       <form class="form-inline" role="form">
+  <div class="form-group">
+    
+    <input type="text" class="form-control" placeholder="username" id="username">
+  </div>
+  <div class="form-group">
+
+    <input type="password" class="form-control" placeholder="password" id="pwd">
+  </div>
+  <button type="submit" class="btn btn-success">Submit</button>
+</form>
+
+            <header>
+                <!-- HEADLINE -->
+                <h1 data-animated="GoIn"><b>SIGN ME IN</b> site is coming soon...</h1>
+            </header>
+
+            <!-- START TIMER -->
+            <div id="timer" data-animated="FadeIn">
+                <p id="message"></p>
+                <div id="days" class="timer_box"></div>
+                <div id="hours" class="timer_box"></div>
+                <div id="minutes" class="timer_box"></div>
+                <div id="seconds" class="timer_box"></div>
+            </div>
+            <!-- END TIMER -->
+            <div class="col-lg-4 col-lg-offset-4 mt centered">
+            	<h4>LET ME KNOW WHEN YOU LAUNCH</h4>
+				<form class="form-inline" role="form">
+				  <div class="form-group">
+				    <label class="sr-only" for="exampleInputEmail2">Email address</label>
+				    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+				  </div>
+				  <button type="submit" class="btn btn-info">Submit</button>
+				</form>       
+
+      
+			</div>
+            
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+        <!-- LAYER OVER THE SLIDER TO MAKE THE WHITE TEXTE READABLE -->
+        <div id="layer"></div>
+        <!-- END LAYER -->
+        <!-- START SLIDER -->
+        <div id="slider" class="rev_slider">
+            <ul>
+              <li data-transition="slideleft" data-slotamount="1" data-thumb="assets/img/slider/1.jpg">
+                <img src="assets/img/slider/1.jpg">
+              </li>
+              <li data-transition="slideleft" data-slotamount="1" data-thumb="assets/img/slider/2.jpg">
+                <img src="assets/img/slider/2.jpg">
+              </li>
+              <li data-transition="slideleft" data-slotamount="1" data-thumb="assets/img/slider/3.jpg">
+                <img src="assets/img/slider/3.jpg">
+              </li>
+              <li data-transition="slideleft" data-slotamount="1" data-thumb="assets/img/slider/4.jpg">
+                <img src="assets/img/slider/4.jpg">
+              </li>
+            </ul>
+        </div>
+        <!-- END SLIDER -->
+    </section>
+    <!-- END HEADER -->
 
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/soon/plugins.js"></script>
+    <script src="assets/js/soon/jquery.themepunch.revolution.min.js"></script>
+    <script src="assets/js/soon/custom.js"></script>
+  </body>
+  <!-- END BODY -->
 </html>
