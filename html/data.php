399a4c6d9fdc138390f0270e738f69d535742fd7
Ryan Challis <ryanchallis1994@gmail.com>
2015-09-25 15:02:20 +0100

    data.php

--- html/data.php
+++ html/data.php
@@ -7,7 +7,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Program: " . $row["Description"]. "<br>";
+        echo "id: " . $row["Id"]. " - Program: " . $row["Description"]. "<br>";
     }
 } else {
     echo "0 results";
