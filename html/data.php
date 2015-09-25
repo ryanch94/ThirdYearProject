27302b62a791e411bad0aa45c1f9976e708da01b
Ryan Challis <ryanchallis1994@gmail.com>
2015-09-25 15:37:24 +0100

    NOW it works

--- html/data.php
+++ html/data.php
@@ -8,11 +8,17 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     echo mysqli_num_rows($result);
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["Id"]. " - Program: " . $row["Description"]. "<br>";
+        "id: " . $row["Id"]. " - Program: " . $row["Description"]. "<br>";
     }
 } else {
     echo "0 results";
 }
+
+echo "<select name='PcID'>";
+while ($row = mysql_fetch_array($result)) {
+    echo "<option value='" . $row['PcID'] . "'>" . $row['PcID'] . "</option>";
+}
+echo "</select>";
 
 mysqli_close($conn);
 ?>
