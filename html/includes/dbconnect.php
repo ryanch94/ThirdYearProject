ee4cb53c3abf32df6337ba625279025979407e9f
Ryan Challis <ryanchallis1994@gmail.com>
2015-09-25 15:30:57 +0100

    NOW it works

--- html/includes/dbconnect.php
+++ html/includes/dbconnect.php
@@ -9,14 +9,9 @@ define("MYSQL_DB", "project"); // Database to be used (currently the 'project' D
 
 //Initialise DB connection using above variables
 $conn = mysqli_connect(MYSQL_ADDRESS, MYSQL_USERNAME, MYSQL_PASS, MYSQL_DB);
-echo 'yes';
 // Check if DB connection was successful, if not kill the rest of the page load and display error
 if (!$conn) {
     die("MySQL Connection failed: " . mysqli_connect_error());
-}
-else
-{
-    echo 'success';
 }
 ?>
 
