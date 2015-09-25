3b8d244dc7fc1f7f530b9bf91460896bd5f9bb51
Ryan Challis <ryanchallis1994@gmail.com>
2015-09-25 15:25:27 +0100

    test

--- html/includes/dbconnect.php
+++ html/includes/dbconnect.php
@@ -8,7 +8,7 @@ define("MYSQL_PASS", "xteUPG8wqQZ8yKzm"); // Password for phpaccess MySQL accoun
 define("MYSQL_DB", "project") // Database to be used (currently the 'project' DB)
 
 //Initialise DB connection using above variables
-global $conn = mysqli_connect(MYSQL_ADDRESS, MYSQL_USERNAME, MYSQL_PASS, MYSQL_DB);
+global $conn = mysqli_connect("localhost", "phpaccess", "xteUPG8wqQZ8yKzm", "project");
 
 // Check if DB connection was successful, if not kill the rest of the page load and display error
 if (!$conn) {
