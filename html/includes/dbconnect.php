54219e869f906358414945249185b33db03cf621
Ryan Challis <ryanchallis1994@gmail.com>
2015-09-25 15:29:50 +0100

    test2

--- html/includes/dbconnect.php
+++ html/includes/dbconnect.php
@@ -5,10 +5,10 @@
 define("MYSQL_ADDRESS", "localhost"); // IP Address of the MySQL server
 define("MYSQL_USERNAME", "phpaccess"); // Username to log into MySQL server
 define("MYSQL_PASS", "xteUPG8wqQZ8yKzm"); // Password for phpaccess MySQL account (randomly generated when user was created)
-define("MYSQL_DB", "project") // Database to be used (currently the 'project' DB)
+define("MYSQL_DB", "project"); // Database to be used (currently the 'project' DB)
 
 //Initialise DB connection using above variables
-$conn = mysqli_connect("localhost", "phpaccess", "xteUPG8wqQZ8yKzm", "project");
+$conn = mysqli_connect(MYSQL_ADDRESS, MYSQL_USERNAME, MYSQL_PASS, MYSQL_DB);
 echo 'yes';
 // Check if DB connection was successful, if not kill the rest of the page load and display error
 if (!$conn) {
