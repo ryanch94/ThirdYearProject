8a063210e1d8ae72a91cc6c98de7a00135cfcaba
Ryan Challis <ryanchallis1994@gmail.com>
2015-11-05 19:20:13 +0000

    Write test web api code

---
+++ SignMeIn/WebApp/Controllers/CourseController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CourseController : ApiController
+    {
+        private Sign_Me_In_DataEntities1 db = new Sign_Me_In_DataEntities1();
+
+        // GET: api/Course
+        public IQueryable<Course> GetCourses()
+        {
+            return db.Courses;
+        }
+
+        // GET: api/Course/5
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> GetCourse(int id)
+        {
+            Course course = await db.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        // PUT: api/Course/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCourse(int id, Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Course
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> PostCourse(Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Courses.Add(course);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = course.Id }, course);
+        }
+
+        // DELETE: api/Course/5
+        [ResponseType(typeof(Course))]
+        public async Task<IHttpActionResult> DeleteCourse(int id)
+        {
+            Course course = await db.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            db.Courses.Remove(course);
+            await db.SaveChangesAsync();
+
+            return Ok(course);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CourseExists(int id)
+        {
+            return db.Courses.Count(e => e.Id == id) > 0;
+        }
+    }
+}
