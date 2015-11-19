0e2a00eeca758e855d04b10bda53a564f2e88f9c
S00147109 <S00147109@mail.itsligo.ie>
2015-11-19 11:49:58 +0000

    added timetable page with basic angular

---
+++ FashionFix/FashionFix/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FashionFix.Startup))]
+namespace FashionFix
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
