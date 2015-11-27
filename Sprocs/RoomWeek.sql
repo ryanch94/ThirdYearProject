ab7f56aec0e49b251bc2e4ef1f609b81dd81f70a
Declan Feehily <s00145584@mail.itsligo.ie>
2015-11-27 13:59:51 +0000

    database work

--- Sprocs/RoomWeek.sql
+++ Sprocs/RoomWeek.sql
@@ -1,11 +1,11 @@
-alter PROC [dbo].[CourseWeek]
-@CourseID int, @weeknumber int
+alter PROC [dbo].[RoomWeek]
+@RoomID int, @weeknumber int
 AS
 
-SELECT Name, ShortName, FirstName, LastName, DayBlock, Code,ClassLength
+SELECT distinct Name, ShortName, FirstName, LastName, DayBlock, Code, ClassLength, StartTime --StartTime has to be returned by distinct
 FROM TimeTable INNER JOIN Module on TimeTable.ModuleId = Module.Id
 INNER JOIN [dbo].[User] on Module.LecturerId = [User].Id
 INNER JOIN Room on TimeTable.RoomId = Room.Id
-WHERE CourseId = @CourseID
+WHERE Room.Id = @RoomID
 AND WeekNumber = @weeknumber
 ORDER BY StartTime,DayBlock
